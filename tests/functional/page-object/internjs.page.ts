f1925f728e96cd3ae0830d542448a1379cea186a
Kamaruddin, Firdaus <firdaus.kamaruddin@mbww.com>
2017-07-11 16:43:13 +0800

    add example to extends base class

---
+++ tests/functional/page-object/internjs.page.ts
@@ -0,0 +1,5 @@
+import { Base } from "./base.page";
+
+export class Intern extends Base {
+
+}
--- tests/functional/spec/test.spec.ts
+++ tests/functional/spec/test.spec.ts
@@ -1,19 +1,19 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
 import * as Test from 'intern/lib/Test';
-import { Base } from "../page-object/base.page";
+import { Intern } from "../page-object/internjs.page";
 
 const suite: Object = {
     name: 'create typescript test',
 
-    setup: function (this: Test) {
-        Base.setBrowser(this.remote);
+    setup(this: Test) {
+        Intern.setBrowser(this.remote);
     },
 
 
     async "inter js page"(this: Test) {
-        await Base.navigate();
-        let internLogo = await Base.internLogo;
+        await Intern.navigate();
+        let internLogo = await Intern.internLogo;
         assert.equal(internLogo, "Intern.", "Cant find intern logo")
         return this.remote;
     }
