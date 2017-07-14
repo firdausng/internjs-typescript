24c789e5032fd3e889538ab0e84b0b8543c3cb23
Kamaruddin, Firdaus <firdaus.kamaruddin@mbww.com>
2017-07-14 15:47:23 +0800

    update file

--- config/config.ts
+++ config/config.ts
@@ -7,17 +7,4 @@ export const env: { [key: string]: { [key: string]: string } } = {
 
 export const find_timeout = 10000;
 
-export const api_key = {
-    "location": "header",
-    "mbwna.test1": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1ODA0NzViNjQwZTA1MDAwMTEyZmQyYjgiLCJpZCI6IjU4MDQ3NWI2NDBlMDUwMDAxMTJmZDJiOCIsInVzZXJuYW1lIjoibWJ3bmEudGVzdDFAbWJ3dy5jb20iLCJlbWFpbCI6Im1id25hLnRlc3QxQG1id3cuY29tIiwiZGlzcGxheU5hbWUiOiJtYndOQSBUZXN0MSIsInJvbGVzIjpbInVzZXIiLCJtYW5hZ2VyIl0sImlhdCI6MTQ4MjkwMTk2Mn0.VQDh6KptiLxu_lqf2XIbAMILz5uIuC1AJySx2b7N1Vw"
-}
-
 export const api_timeout = 5000;
-
-
-export const users = {
-    "test1": {
-        "email": "mbwna.test1@mbww.com",
-        "password": "Get2TheChopp@96"
-    }
-}
